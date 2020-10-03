fa335c0134ab4aae1f17ba6b15576f5d868418ee
Abed Qasim <abedqassem_r_998@hotmail.com>
2020-10-03 21:53:44 +0300

    Initial Commit

---
+++ Resturant/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Resturant
+//
+//  Created by Abed Qassim on 10/3/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
